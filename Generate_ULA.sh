207f46c5fd6221964ded5c0402ac62653e007682
MartineauUK <MartineauUK@users.noreply.github.com>
2022-03-20 07:23:26 +0000

    Update Generate_ULA.sh
    CHANGE: Display Dual-stack IPv4+IPv6 alternative based on SNB Forum member @heysoundude's reference article

--- Generate_ULA.sh
+++ Generate_ULA.sh
@@ -8,14 +8,14 @@ SayT() {
 # see https://www.digitalocean.com/community/tutorials/how-to-set-up-wireguard-on-ubuntu-20-04
 
 if [ ! -f /opt/bin/date ];then
-	echo -e "\a\n\t*** ERROR Requires Entware 'date' module....ABORTing\n"
-	exit 99
-	
+    echo -e "\a\n\t*** ERROR Requires Entware 'date' module....ABORTing\n"
+    exit 99
+
 fi
 
 # From time+EUI-64 as per RFC 4193 https://www.rfc-editor.org/rfc/rfc4193#section-3.2.2
 
-# Pre-reqs		Entware date (coreutils-date) 
+# Pre-reqs      Entware date (coreutils-date)
 
 [ ! -f /opt/bin/date ] && { SayT "*** ERROR Requires Entware 'date' module....ABORTing\n"; return 1 ;}
 
@@ -33,7 +33,8 @@ HASH=$(openssl dgst -sha1 /tmp/wgm_ula | awk '{print $2}' | cut -c 31- )
 
 IPV6="fd"${HASH:0:2}:${HASH:2:4}:${HASH:6:4}"::/64"
 
+# https://blogs.infoblox.com/ipv6-coe/ula-is-broken-in-dual-stack-networks/
 SayT "Here is your IPv6 ULA based on this hardware's MACs IPV6="$IPV6
-echo -e "\n\tHere is your IPv6 ULA based on this hardware's MACs IPV6="$IPV6"\n"
+echo -e "\n\tHere is your IPv6 ULA based on this hardware's MACs IPV6="$IPV6" (Use "aa"${HASH:0:2}:${HASH:2:4}:${HASH:6:4}"::/64 for Dual-stack IPv4+IPv6")\n"
 
 rm /tmp/wgm_ula 2>/dev/null
